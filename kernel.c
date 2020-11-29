4d72898d84d10f32c952cf9977c5ddd799736b77
Max Breitenfeldt <git@mxbr.me>
2020-11-29 12:19:16 +0100

    Implement memory remap

--- kernel.c
+++ kernel.c
@@ -3,7 +3,14 @@
 #include "stdio.h"
 #include "string.h"
 
+void memory_remap(void) {
+  AT91C_BASE_MC->MC_RCR = AT91C_MC_RCB;
+}
+
 void kernel(void) {
+  memory_remap();
+  memcpy((void*)0x4, ivt, sizeof(ivt));
+
   printf("Send x to demonstrate printf-formatting\n");
   while (1) {
     char c = (char)getchar();
