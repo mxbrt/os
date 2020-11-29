094e2eadaa34480beeeaa1fa19b5ed099fc4b65c
Max Breitenfeldt <git@mxbr.me>
2020-11-29 16:35:45 +0100

    Add exception demo

--- kernel.c
+++ kernel.c
@@ -19,5 +19,17 @@ void kernel(void) {
       const char *str = "Format demo";
       printf("%s: %p %x\n", str, (void*)0xAFFE, 0xAFFE);
     }
+    if (c == 'u') {
+      printf("Undefined exception: Dereferencing null-pointer\n");
+      *(volatile int*)0x0 = 1;
+    }
+    if (c == 'a') {
+      printf("Data abort: Dereferencing invalid memory\n");
+      *(volatile int*)0xFFFFFFFF = 1;
+    }
+    if (c == 's') {
+      printf("Emit syscall(0x1)\n");
+      __asm volatile("svc 0x1"); \
+    }
   }
 }
