0c9a290f6af686b42a449cc74862ca2c30a38168
Max Breitenfeldt <git@mxbr.me>
2020-12-12 17:00:37 +0100

    Add exception_{enable,disable}

--- arch.c
+++ arch.c
@@ -3,10 +3,30 @@
 #include "string.h"
 #include "time.h"
 #include "panic.h"
+#include "arch.h"
 
 #define SCLK_FREQUENCY 32768
 
 void init(void) {
+int get_cpsr(void);
+void set_cpsr(int);
+
+typedef enum {
+  CPSR_IRQ = 1 << 7
+} CPSR;
+
+void enable_interrupts(void) {
+  int cpsr = get_cpsr();
+  cpsr &= ~CPSR_IRQ;
+  set_cpsr(cpsr);
+}
+
+void disable_interrupts(void) {
+  int cpsr = get_cpsr();
+  cpsr |= CPSR_IRQ;
+  set_cpsr(cpsr);
+}
+
   // memory remap
   AT91C_BASE_MC->MC_RCR = AT91C_MC_RCB;
 
--- arch.h
+++ arch.h
@@ -1,6 +1,8 @@
 #ifndef __ARCH_H_
 #define __ARCH_H_
 
+void enable_interrupts(void);
+void disable_interrupts(void);
 void init(void);
 
 #endif
