35ce1304ac2929798b12c2e64f047c2e61f7a087
Max Breitenfeldt <git@mxbr.me>
2021-01-18 17:54:35 +0100

    Fix: struct initialization for dbgu fifo

--- arch/dbgu.c
+++ arch/dbgu.c
@@ -4,7 +4,12 @@
 #include <fifo.h>
 #include <stdio.h>
 
-fifo_t rx_fifo = {0};
+fifo_t rx_fifo = {
+  .buf = {0},
+  .rx = 0,
+  .tx = 0,
+  .cnt = 0,
+};
 
 void init_dbgu(void) {
   AT91C_BASE_DBGU->DBGU_IER = AT91C_US_RXRDY;
