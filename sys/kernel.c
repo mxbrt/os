8ae1f47298731fa77ccf5a0f447004afd9553059
Max Breitenfeldt <git@mxbr.me>
2021-01-23 14:33:20 +0100

    Improve idle thread
    The idle thread only gets executed when no other threads are scheduled.

--- sys/kernel.c
+++ sys/kernel.c
@@ -5,16 +5,9 @@
 #include <thread.h>
 #include <panic.h>
 
-void kernel_thread(void) {
-  while (1) {
-    // TODO: enable sleep mode
-  }
-}
-
 void kernel(void) {
   init();
   scheduler_init();
-  thread_init(&kernel_thread);
   // the scheduler is called from the interrupt context, wait for first tick
   enable_interrupts();
   while(1) {}
--- sys/thread.c
+++ sys/thread.c
@@ -6,6 +6,7 @@
 #include <syscall.h>
 #include <thread.h>
 
+tcb_t idle_thread;
 tcb_t threads[N_THREADS];
 list_t thread_list[N_THREADS];
 list_t *thread_list_head;
@@ -13,6 +14,22 @@ list_t *thread_list_head;
 // Threads
 unsigned int get_spsr(void);
 void set_spsr(unsigned int);
+
+void idle_thread_fn(void) {
+  while (1) {
+    // TODO: enable sleep mode
+  }
+}
+
+void thread_tcb_init(tcb_t* tcb, void *fn) {
+  memset(tcb, 0, sizeof(*tcb));
+  tcb->state = THREAD_START;
+  tcb->regs[13] = (size_t)&tcb->stack[THREAD_STACK_SIZE - sizeof(size_t)];
+  tcb->regs[14] = (size_t)&syscall_exit;
+  tcb->regs[15] = (size_t)fn + 0x4;
+  tcb->SPSR = 0x1F |               // Set system mode
+              (1 << 6);            // disable fiq
+}
 
 int thread_init(void *fn) {
   // find free tcb slot
@@ -33,28 +50,21 @@ int thread_init(void *fn) {
   list_t *thread_node = &thread_list[free_idx];
   list_init(thread_node, tcb);
 
+  thread_tcb_init(tcb, fn);
+
   if (thread_list_head == NULL) {
     thread_list_head = thread_node;
   } else {
     list_insert(thread_list_head, thread_node);
   }
-
-  memset(tcb, 0, sizeof(*tcb));
-  tcb->state = THREAD_START;
-  tcb->regs[13] = (size_t)&tcb->stack[THREAD_STACK_SIZE - sizeof(size_t)];
-  tcb->regs[14] = (size_t)&syscall_exit;
-  tcb->regs[15] = (size_t)fn + 0x4;
-  tcb->SPSR = 0x1F |               // Set system mode
-              (1 << 6);            // disable fiq
   return 0;
 }
 
-unsigned int* thread_load_head(void) {
+unsigned int* thread_load(tcb_t* tcb) {
   // set next thread context
-  tcb_t *thread = thread_list_head->data;
-  set_spsr(thread->SPSR);
-  thread->state = THREAD_RUNNING;
-  return thread->regs;
+  set_spsr(tcb->SPSR);
+  tcb->state = THREAD_RUNNING;
+  return tcb->regs;
 }
 
 unsigned int* thread_finish(void) {
@@ -65,9 +75,14 @@ unsigned int* thread_finish(void) {
   memset(cur_thread, 0, sizeof(*cur_thread));
   cur_thread->state = THREAD_FINISH;
 
-  thread_list_head = thread_list_head->next;
-  list_remove(thread_list_head->prev);
-  return thread_load_head();
+  if (thread_list_head->next == thread_list_head) {
+    thread_list_head = NULL;
+    return thread_load(&idle_thread);
+  } else {
+    thread_list_head = thread_list_head->next;
+    list_remove(thread_list_head->prev);
+    return thread_load(thread_list_head->data);
+  }
 }
 
 // Scheduler
@@ -75,11 +90,12 @@ void scheduler_init(void) {
   memset(thread_list, 0, sizeof(thread_list));
   memset(threads, 0, sizeof(threads));
   thread_list_head = NULL;
+  thread_tcb_init(&idle_thread, &idle_thread_fn);
 }
 
 unsigned int* scheduler_tick(unsigned int regs[16]) {
   if (thread_list_head == NULL) {
-    PANIC("NO THREADS");
+    return thread_load(&idle_thread);
   }
 
   if (thread_list_head->next != thread_list_head) {
@@ -95,9 +111,8 @@ unsigned int* scheduler_tick(unsigned int regs[16]) {
     memcpy(cur_thread->regs, regs, 16*4);
   }
   cur_thread->state = THREAD_WAITING;
-  return thread_load_head();
+  return thread_load(thread_list_head->data);
 }
-
 
 // Threads
 void thread_echo(void) {
